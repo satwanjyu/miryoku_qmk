8ef4fd3fba19c77cf08c1e5cc352187d0ed6ca07
satwanjyu <satwanjyu@outlook.com>
2024-06-13 09:16:06 +0000

    customise tap layer layout to gaming-focused

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,3 +5,17 @@
 
 #pragma once
 
+#if defined KEYBOARD_bastardkb_charybdis_3x5_v2_splinky_3
+#define MIRYOKU_LAYERMAPPING_TAP( \
+K00,     K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09, \
+K10,     K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19, \
+K20,     K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29, \
+N30,     N31,   K32,   K33,   K34,          K35,   K36,   K37,   N38,   N39 \
+) \
+LAYOUT_charybdis_3x5( \
+KC_ESC,  K00,   K01,   K02,    K03,          K06,     K07,     KC_UP,   K09,     KC_BSPC, \
+KC_LSFT, K10,   K11,   K12,    K13,          K16,     KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,  \
+KC_LCTL, K20,   K21,   K22,    K23,          K26,     K27,     K28,     K29,     DF(U_BASE),  \
+                KC_G,  KC_SPC, KC_TAB,       KC_LGUI, KC_RALT \
+)
+#endif
--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,5 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+MIRYOKU_TAP = QWERTY
+AUTO_SHIFT_ENABLE = no
